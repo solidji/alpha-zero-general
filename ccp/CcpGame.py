3b795c653e710c66bfc0f8259f3f4999a8dba0f7
计炜 <jw@appgame.com>
2018-11-29 21:24:31 +0800

    通过记录恢复一局牌

--- ccp/CcpGame.py
+++ ccp/CcpGame.py
@@ -1,6 +1,7 @@
 from Game import Game
 import copy
-from .CcpLogic import Poker,Player,PlayRecords,get_state,action_dict
+from .CcpLogic import Poker,Player,PlayRecords,get_state
+from ccp.actions import  action_dict
 
 class CcpGame(Game):
     def __init__(self):
@@ -11,7 +12,7 @@ class CcpGame(Game):
         # 用records来表示状态，但保存和传给神经网络训练的，要从records中抽出state
         g = Poker()
         #洗牌、发牌、设置地主
-        g.game_start(train=True)
+        g.game_start()
 
         # state = get_state(g.playrecords, g.players[0])
         return g.playrecords
@@ -21,17 +22,15 @@ class CcpGame(Game):
         return (6, 15)
 
     def getActionSize(self):
-        # 这里返回值用于确定神经网络的Pi输出域，为所有可能的出牌组合+buyao
-        return len(action_dict)+1
+        # 这里返回值用于确定神经网络的Pi输出域，为所有可能的出牌组合 + 429 buyao + 430 yaobuqi
+        return len(action_dict)+2
 
     def getNextState(self, board, player, action):
         g = Poker()
-        # g.initFromRecords(board)
-        g.playrecords = board
-        g.players = player
+        g.initFromRecords(board)
         g.execute_move(action, player)
 
-        return (g, (player+1)%3-1)
+        return (g.playrecords, g.i)
 
 
 
